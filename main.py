ec06970da42212ec3ba72c1c381fd9b4c9d4d2d6
dhruv50ae <linkdp2012@gmail.com>
2022-04-09 21:23:30 +0100

    Functions with params

---
+++ main.py
@@ -0,0 +1,17 @@
+def greet():
+    print("Hello1")
+    print("Hello2")
+    print("Hello3")
+
+
+greet()
+
+
+def greetWithName(name):
+    print(f"Hello1 {name}")
+    print(f"Hello2 {name}")
+    print(f"Hello3 {name}")
+
+
+greetWithName("Jeff")
+
