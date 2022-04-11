e06ea43a207fb2f79c84e498d3fb176dd8e8ff08
dhruv50ae <linkdp2012@gmail.com>
2022-04-11 11:58:09 +0100

    Created cipher

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def primeChecker(number):
-    isPrime = True
-
-    for i in range(2, number):
-        if number % i == 0:
-            isPrime = False
-    if isPrime:
-        print("That is indeed a prime number")
-    else:
-        print("That is not a prime")
+direction = input("Type 'encode' to encrypt, or 'decode to decrypt\n'")
+text = input("Type your message: ").lower()
+shift = int(input("Type the shift number: "))
 
 
-n = int(input("Check this number: "))
-primeChecker(number=n)
+def encrypt(plainText, shiftAmount):
+    cipherText = ""
+
+    for letter in plainText:
+        position = alphabet.index(letter)
+        newPosition = position + shiftAmount
+        newLetter = alphabet[newPosition]
+        cipherText += newLetter
+    print(f"Your ecrypted message is: {cipherText}")
+
+
+encrypt(plainText=text, shiftAmount=shift)
